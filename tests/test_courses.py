5faaa5277f7ad54e5e7c39a498e5da760dd1fdde
ttera <4amatterasu@gmail.com>
2025-08-01 19:02:33 +0300

    Знакомство с PageComponent.Практика работы с Page Component

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -10,8 +10,11 @@ def test_empty_courses_list(courses_list_page: CoursesListPage):
 
     courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-    courses_list_page.check_visible_courses_title()
+    courses_list_page.navbar.check_visible("username")
+    courses_list_page.sidebar.check_visible()
 
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
     courses_list_page.check_visible_empty_view()
 
 
