1ea873bf06b291ab1220a319206bb9c65be17016
ttera <4amatterasu@gmail.com>
2026-07-07 15:14:17 +0300

    Настройки автотестов с Pydantic.Практика работы с настройками

--- tests/authentication/test_authorization.py
+++ tests/authentication/test_authorization.py
@@ -2,6 +2,7 @@ import allure
 import pytest
 from allure_commons.types import Severity
 
+from config import settings
 from fixtures.pages import registration_page, dashboard_page
 from pages.authentication.login_page import LoginPage
 from pages.authentication.registration_page import RegistrationPage
@@ -10,6 +11,7 @@ from tools.allure.epics import AllureEpic
 from tools.allure.features import AllureFeature
 from tools.allure.stories import AllureStory
 from tools.allure.tags import AllureTag
+from tools.routes import AppRoute
 
 
 @pytest.mark.regression
@@ -34,7 +36,7 @@ class TestAuthorization:
     @allure.title("User login with wrong email or password")
     @allure.severity(Severity.CRITICAL)
     def test_wrong_email_or_password_authorization(self, dashboard_page: DashboardPage, login_page: LoginPage, email: str, password: str):
-        login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+        login_page.visit(AppRoute.LOGIN)
         login_page.login_form.fill(email=email, password=password)
         login_page.click_login_button()
         login_page.check_visible_wrong_email_or_password_alert()
@@ -43,21 +45,28 @@ class TestAuthorization:
     @allure.title("User login with correct email and password")
     @allure.severity(Severity.BLOCKER)
     def test_successful_authorization(self, registration_page: RegistrationPage, dashboard_page: DashboardPage, login_page: LoginPage):
-        registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-        registration_page.registration_form.fill(email="user.name@gmail.com", username="username", password="password")
+        registration_page.visit(AppRoute.REGISTRATION)
+        registration_page.registration_form.fill(
+            email=settings.test_user.email,
+            username=settings.test_user.username,
+            password=settings.test_user.password
+        )
         registration_page.click_registration_button()
 
         dashboard_page.dashboard_toolbar_view.check_visible()
-        dashboard_page.navbar.check_visible("username")
+        dashboard_page.navbar.check_visible(settings.test_user.username)
         dashboard_page.sidebar.check_visible()
 
         dashboard_page.sidebar.click_logout()
 
-        login_page.login_form.fill(email="user.name@gmail.com", password="password")
+        login_page.login_form.fill(
+            email=settings.test_user.email,
+            password=settings.test_user.password
+        )
         login_page.click_login_button()
 
         dashboard_page.dashboard_toolbar_view.check_visible()
-        dashboard_page.navbar.check_visible("username")
+        dashboard_page.navbar.check_visible(settings.test_user.username)
         dashboard_page.sidebar.check_visible()
 
     @allure.tag(AllureTag.NAVIGATION)
@@ -68,7 +77,11 @@ class TestAuthorization:
             login_page: LoginPage,
             registration_page: RegistrationPage
     ):
-        login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+        login_page.visit(AppRoute.LOGIN)
         login_page.click_registration_link()
 
-        registration_page.registration_form.check_visible(email="", username="", password="")
+        registration_page.registration_form.check_visible(
+            email="",
+            username="",
+            password=""
+        )
--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -2,6 +2,7 @@ import allure
 import pytest
 from allure_commons.types import Severity
 
+from config import settings
 from fixtures.pages import courses_list_page
 from pages.courses.courses_list_page import CoursesListPage
 from pages.courses.create_course_page import CreateCoursePage
@@ -9,6 +10,7 @@ from tools.allure.epics import AllureEpic
 from tools.allure.features import AllureFeature
 from tools.allure.stories import AllureStory
 from tools.allure.tags import AllureTag
+from tools.routes import AppRoute
 
 
 @pytest.mark.courses
@@ -24,11 +26,9 @@ class TestCourses:
     @allure.title("Check displaying of empty courses list")
     @allure.severity(Severity.NORMAL)
     def test_empty_courses_list(self, courses_list_page: CoursesListPage):
-        courses_list_page.visit(
-            "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses"
-        )
+        courses_list_page.visit(AppRoute.COURSES)
 
-        courses_list_page.navbar.check_visible("username")
+        courses_list_page.navbar.check_visible(settings.test_user.username)
         courses_list_page.sidebar.check_visible()
 
         courses_list_page.toolbar_view.check_visible()
@@ -37,9 +37,7 @@ class TestCourses:
     @allure.title("Create course")
     @allure.severity(Severity.CRITICAL)
     def test_create_course(self, create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
-        create_course_page.visit(
-            "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create"
-        )
+        create_course_page.visit(AppRoute.CREATE_COURSES)
 
         create_course_page.create_course_toolbar_view.check_visible(is_create_course_disabled=True)
 
@@ -53,7 +51,7 @@ class TestCourses:
             description='Click on "Create exercise" button to create new exercise'
         )
 
-        create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
+        create_course_page.image_upload_widget.upload_preview_image(settings.test_data.image_png_file)
         create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
 
         create_course_page.fill_create_course_form()
@@ -76,12 +74,10 @@ class TestCourses:
             create_course_page: CreateCoursePage,
             courses_list_page: CoursesListPage
     ):
-        create_course_page.visit(
-            "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create"
-        )
+        create_course_page.visit(AppRoute.CREATE_COURSES)
 
         create_course_page.fill_create_course_form()
-        create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
+        create_course_page.image_upload_widget.upload_preview_image(settings.test_data.image_png_file)
         create_course_page.create_course_toolbar_view.click_create_course_button()
 
         courses_list_page.course_view.check_visible(
