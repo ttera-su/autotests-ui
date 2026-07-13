75233436d8106f78bd8150d3abfa448e06eb894d
ttera <4amatterasu@gmail.com>
2026-07-13 13:32:24 +0300

    Логирование взаимодействий со страницей.Практика работы с логированием

--- components/base_component.py
+++ components/base_component.py
@@ -2,10 +2,15 @@ from typing import Pattern
 
 from playwright.sync_api import Page, expect
 
+from pages.base_page import logger
+
 
 class BaseComponent:
     def __init__(self, page: Page):
         self.page = page
 
     def check_current_url(self, expected_url: Pattern[str]):
+        step = f'Checking that current url matches pattern "{expected_url.pattern}"'
+
+        logger.info(step)
         expect(self.page).to_have_url(expected_url)
--- tools/playwright/pages.py
+++ tools/playwright/pages.py
@@ -2,6 +2,8 @@ import allure
 from playwright.sync_api import Playwright, Page
 
 from config import settings, Browser
+from tools.playwright.mocks import mock_static_resources
+
 
 def initialize_playwright_page(
         playwright: Playwright,
@@ -17,6 +19,7 @@ def initialize_playwright_page(
     )
     context.tracing.start(screenshots=True, snapshots=True, sources=True)
     page = context.new_page()
+    mock_static_resources(page)
 
     yield page
 
